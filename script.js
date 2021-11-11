78e66017c9e8fb51413b42e25b180ddeb2d85328
Robert Blue <robert@rbctech.com>
2021-11-10 18:29:40 -0800

    Bunch'o'tweaks

--- script.js
+++ script.js
@@ -8,22 +8,29 @@ async function getWAXPrice() {
     return data.wax.usd;
 }
 
-async function populatePage() {
-    // Reset the table
-    exchangeTable.innerHTML = '';
-    
+async function populatePage() {   
     const now = new Date();
     document.getElementById('timestamp').innerText = now.toLocaleTimeString();
 
     const waxPrice = await getWAXPrice();
     document.getElementById("waxPrice").innerText = waxPrice
 
-    for (const templateID of templateIDs) {
-        console.log("TEMPLATE: ", templateID)
+    const results = []
+
+    for (var i = 0; i < templateIDs.length; i++) {
+        const templateID = templateIDs[i]
+        document.getElementById("refreshStatus").innerText = `retrieving template ${i}/${templateIDs.length}`
+
         const url = `https://wax.api.atomicassets.io/atomicmarket/v1/sales/templates?symbol=WAX&template_id=${templateID}&order=asc&sort=price`;
         const response = await fetch(url);
         const data = await response.json();
+        results.push(data)
+    }
 
+    // Reset the table
+    exchangeTable.innerHTML = '';
+
+    for (data of results) {
         for (const d of data.data) {
             console.log("DATA:", d)
 
@@ -36,29 +43,38 @@ async function populatePage() {
             const collectionName = d.collection_name
             const schemaName = d.assets[0].schema.schema_name
             const cardName = d.assets[0].name
+            const templateId = d.assets[0].template.template_id
 
             const salesLink = `https://wax.atomichub.io/market/history?collection_name=${collectionName}&data:text.name=${cardName}&order=desc&schema_name=${schemaName}&sort=updated&symbol=WAX`
             const saleLink = `https://wax.atomichub.io/market/sale/${saleId}`
             const listingsLink = `https://wax.atomichub.io/market?collection_name=${collectionName}&data:text.name=${cardName}&order=asc&schema_name=${schemaName}&sort=price&symbol=WAX`
+            const collectionLink = `https://wax.atomichub.io/explorer/collection/${collectionName}`
+            const templateLink = `https://wax.atomichub.io/explorer/template/${collectionName}/${templateId}`
+            const inventoryLink = `https://wax.atomichub.io/profile/${waxAddress}?collection_name=${collectionName}&match=${cardName}&order=desc&sort=transferred`
 
             const output = `
-            <tr>
-                <td style="color: ${collectionName.toHSL()}">${collectionName}</td>
-                <td style="color: darkcyan">${cardName}</td>
-                <td>
-                    <span style="color: orange">${Math.round(price * 100) / 100}</span> WAX
-                </td>
-                <td>
-                    $<span  style="color: green">${(price * waxPrice).toFixed(2)}</span>
-                </td>
-                <td>
-                <a href="${saleLink}" target="_blank">buy</a> | <a href="${salesLink}" target="_blank">sales</a>  | <a href="${listingsLink}" target="_blank">listings</a>
-                </td>
-            </tr>`
+<tr>
+    <td><a href="${templateLink}" class="templateId" target="_blank">${templateId}</a></td>
+    <td><a href="${collectionLink}" target="_blank" style="color: ${collectionName.toHSL()}">${collectionName}</a> - <span class="cardName">${cardName}</span></td>
+    <td>
+        <span style="color: orange">${Math.round(price * 100) / 100}</span> WAX
+    </td>
+    <td>
+        $<span  style="color: green">${(price * waxPrice).toFixed(2)}</span>
+    </td>
+    <td>
+        <a href="${saleLink}" target="_blank">buy</a> 
+        | <a href="${salesLink}" target="_blank">sales</a> 
+        | <a href="${listingsLink}" target="_blank">listings</a>
+        | <a href="${inventoryLink}" target="_blank">${waxAddress}</a>
+    </td>
+</tr>`
 
             exchangeTable.insertAdjacentHTML('afterbegin', output)
         }
     }
+
+    document.getElementById("refreshStatus").innerText = ""
 
     setTimeout(populatePage, refreshInterval)
 }
--- settings.js
+++ settings.js
@@ -1,5 +1,8 @@
 // List of template IDs of NFTs to monitor
-const templateIDs = [345393, 338018, 178062, 167345, 140918, 167332, 167320, 167252, 45017, 329691, 329688, 329692, 251305, 281655, 166103].sort()
+const templateIDs = [348878, 345393, 167345, 140918, 167332, 45017, 329691, 251305, 281655, 166103, 103370, 258614, 145826, 297139, 297137, 285053, 234120].sort()
 
 // Frequency in milliseconds to update the data
 const refreshInterval = 5 * 60 * 1000; // 5 minutes
+
+// WAX Wallet address for creating the "inventory" link
+const waxAddress = 'mo.xy'
